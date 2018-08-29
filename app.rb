59866f8c3d7c4690be8af07d28269ecdc3b1cd83
Jason Viger <jason.viger@gmail.com>
2018-08-29 09:38:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,8 +26,8 @@ class App < Sinatra::Base
     else
       "Session value has not been set!"
     end
-    session['foo'] == 'hello'
-    @session = session
+    session["foo"] ='hello'
+    
   end
 
   get '/fetch' do
